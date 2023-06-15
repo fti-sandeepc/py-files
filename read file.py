e65302cd444311e6b26e0bbdcd57bb37e98488db
Sandie11 <87668502+Sandie11@users.noreply.github.com>
2023-06-15 15:53:14 +0530

    Update read file.py

--- read file.py
+++ read file.py
@@ -5,3 +5,14 @@ con = fs.read()
 print(con)  
 fs.close() 
 welcome to new file
+--------------
+import os
+current_path = os.getcwd()
+print("The current working directory is as follows:")
+print(current_path)
+
+relative_path = "AnotherDirectory\\SubDirectory"
+new_path = os.path.join(os.getcwd(),relative_path)
+print("The new path is as follows:")
+print(new_path)
+-----------------------
